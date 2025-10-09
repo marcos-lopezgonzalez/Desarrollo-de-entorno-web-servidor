ca92cefda72ac5a99127f37607f48d632b0c390d
Marcos López González <2005052@alu.murciaeduca.es>
2025-10-09 16:57:45 +0200

    Mostrar tabla con libros en BBDD

--- Tema02/2_formularios/form_6_libros/index.php
+++ Tema02/2_formularios/form_6_libros/index.php
@@ -1,16 +1,62 @@
+<?php
+include_once "includes/funciones.php";
+include_once "modelo/libro.php";
+?>
+
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
     <title>Libros Marcos López González</title>
 </head>
+
 <body>
     <?php include('menu.php'); ?>
     <main>
+        <?php
+        $listaLibros = obtenerLibros();
 
+        if (count($listaLibros) == 0) {
+            //No existen libros
+            echo "<p>No existen libros</p>";
+        } else {
+            echo "<table class=\"tabla-libros\">";
+            echo "<tr>
+                    <th>Portada</th>
+                    <th>Título</th>
+                    <th>Autor</th>
+                    <th>Año</th>
+                    <th>Géneros</th>
+                  </tr>";
+
+            foreach ($listaLibros as $libro) {
+                echo "<tr>";
+                //Portada
+                echo "<td><img src=\"$libro->portada\"</td>";
+                //Titulo
+                echo "<td>$libro->titulo</td>";
+                //Autor
+                echo "<td>$libro->autor</td>";
+                //Año
+                echo "<td>$libro->ano</td>";
+                //Genero/s
+                echo "<td>";
+                for ($i = 0; $i < count($libro->genero); $i++) {
+                    echo "{$libro->genero[$i]}";
+                    echo "<br>";
+                }
+                echo "</td>";
+                echo "</tr>";
+            }
+
+            echo "</table>";
+        }
+        ?>
     </main>
     <?php include('footer.php'); ?>
 </body>
+
 </html>
